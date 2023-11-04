b7ce55f8732afe5e72dae0a681221790b8c6aebe
avgust <avgust1ne.k6@gmail.com>
2023-11-04 17:58:09 +0600

    Initial commit

---
+++ src/components/Text.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello, React.js!</h1>
+            <p>Welcome to the world of React.js</p>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/images/logo.jsx
@@ -0,0 +1,6 @@
+import reactLogo from "./reactLogo.png";
+
+export default function App() {
+
+    return <img src={reactLogo} alt="react logo" />
+}
