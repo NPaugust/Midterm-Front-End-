c15609b0010ca20dc33610f92783f3be61c890df
avgust <avgust1ne.k6@gmail.com>
2023-11-05 14:46:11 +0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -99,7 +99,7 @@ class Game extends React.Component {
     if (winner) {
       status = 'Winner: ' + winner;
     } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      status = 'Next move for: ' + (this.state.xIsNext ? 'X' : 'O');
     }
 
     return (
